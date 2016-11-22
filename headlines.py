f55859364c9feee0f466c20868677ee7ad4e831b
Dave Phillips <crisponions@gmail.com>
2016-11-22 04:33:29 +0000

    weather

--- headlines.py
+++ headlines.py
@@ -12,21 +12,33 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
             } 
+DEFAULTS = {'publication':'bbc',
+             'city': 'London,UK'}
 
 @app.route('/')
-def get_news():
-    query = request.args.get('publication')
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication: 
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or defaut
+    city = request.args.get('city')
+    if not city:    
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template('home.html', articles=articles, weather=weather)
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = 'bbc'
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather("London,UK")
-    return render_template("home.html", articles=feed['entries'],weather=weather)
+    return feed['entries']
 
 def get_weather(query):
-    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=889b61e24fccd317a493e8929428ee57'
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=889b61e24fccd317a493e8929428ee57'
     query = urllib.quote(query)
     url = api_url.format(query)
     data = urllib2.urlopen(url).read()
@@ -35,7 +47,9 @@ def get_weather(query):
     if parsed.get("weather"):
         weather = {"description":parsed["weather"][0]["description"],
                    "temperature":parsed["main"]["temp"],
-                   "city":parsed["name"]}
+                   "city":parsed["name"],
+                   "country":parsed["sys"]["country"]}
+
 
     return weather
 
